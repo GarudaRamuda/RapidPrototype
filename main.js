2580ab31150c4fdbe46fcdd128063bd794c0750d
SalilPT <salilnyla@gmail.com>
2022-05-09 17:48:19 -0700

    Added basic scene classes

---
+++ main.js
@@ -0,0 +1,7 @@
+"use strict";
+
+let gameConfig = { 
+    type: Phaser.WEBGL,
+
+    scene: [Title, Victory, Message]
+}
---
+++ src/scenes/Message.js
@@ -0,0 +1,5 @@
+class Message extends Phaser.Scene {
+    constructor() {
+        super("messageScene");
+    }
+}
