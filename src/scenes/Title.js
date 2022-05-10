ff8faf54438337b638c8494b3219ba49c71d1237
Jonathan Lewis <jontyjlewis@gmail.com>
2022-05-09 18:25:37 -0700

    added title sheet

--- src/scenes/Title.js
+++ src/scenes/Title.js
@@ -2,4 +2,12 @@ class Title extends Phaser.Scene {
     constructor() {
         super("titleScene");
     }
+
+    preload() {
+        this.load.image('title', './assets/roly-poly-title-sheet.png');
+    }
+
+    create() {
+        let bg = this.add.sprite(100, 100, 'title').setOrigin(0, 0);
+    }
 }
