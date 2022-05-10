365bef3cf820be5e0cfe506b92d3306638c1ff28
Jonathan Lewis <jontyjlewis@gmail.com>
2022-05-09 18:05:51 -0700

    updated main

--- src/main.js
+++ src/main.js
@@ -1,17 +1,28 @@
-const { Phaser } = require("../lib/phaser")
-
 let canvasStyle = " \
 border-radius: 10px; \
 "
 
-let config = {
-    type: Phaser.CANVAS,
-    width: 768, // street.png width * export resize factor
-    height: 432,
-    scene : [Play],
-    backgroundColor : "#1982FC",
-    canvasStyle: canvasStyle
+// let config = {
+//     type: Phaser.CANVAS,
+//     width: 768, // street.png width * export resize factor
+//     height: 432,
+//     scene : [Play],
+//     backgroundColor : "#1982FC",
+//     canvasStyle: canvasStyle
     
+// }
+
+let config = {
+    type: Phaser.AUTO,
+    scale: {
+        parent: 'phaser-example',
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.Center_BOTH,
+        width: 480,
+        height: 320
+    },
+    scene: [Title, Play, Final]
 }
 
 let game = new Phaser.game(config);
+
