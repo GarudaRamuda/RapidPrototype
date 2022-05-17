03171fa7ad568905e84c39fb23ea8d053c3bc517
SalilPT <salilnyla@gmail.com>
2022-05-16 18:12:07 -0700

    Added more text to gameplay scene

--- src/scenes/Gameplay.js
+++ src/scenes/Gameplay.js
@@ -10,12 +10,12 @@ class Gameplay extends Phaser.Scene {
     }
 
     create() {
-        this.loseTransitionText = this.add.text(32, 32, "Press L to lose!");
+        this.loseTransitionText = this.add.text(32, 32, "This is the gameplay scene.\nTap/click to win!\nPress L to lose!", {fontSize: "24px"});
         this.input.on("pointerdown", () => {
             this.scene.start(this.targetSceneKey, {messageIndex: this.currMessageIndex});
         });
         this.input.keyboard.on("keydown-L", () => {
             this.scene.start("lossScene");
-        }, this) 
+        }, this);
     }
 }
