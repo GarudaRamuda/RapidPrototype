ae4d8bbc618cb03b02928760997b16780fc55614
stbchen <stbchen@ucsc.com>
2022-05-16 18:14:00 -0700

    added victory scene picture

--- src/scenes/Victory.js
+++ src/scenes/Victory.js
@@ -2,4 +2,12 @@ class Victory extends Phaser.Scene {
     constructor() {
         super("victoryScene");
     }
+
+    preload () {
+        this.load.image('victory', './assets/victory.png');
+    }
+
+    create () {
+        this.add.image(game.config.width/2, game.config.height/2, 'victory');
+    }
 }
