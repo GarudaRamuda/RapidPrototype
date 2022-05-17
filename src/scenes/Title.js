cd374ee96a828c3650f9199950f85bed0c52c177
SalilPT <salilnyla@gmail.com>
2022-05-16 17:44:59 -0700

    Moved preload method of Title.js

--- src/scenes/Title.js
+++ src/scenes/Title.js
@@ -4,21 +4,20 @@ class Title extends Phaser.Scene {
     
     }
 
-    create() {
-        this.titleTextConfig = {
-    
-        }
-        this.input.on("pointerdown", () => {
-            this.scene.start("messageScene", {messageIndex: 0});
-        });
-
-    }
-
     preload() {
         this.load.image('title', './assets/roly-poly-title-sheet.png');
     }
 
     create() {
+        this.titleTextConfig = {
+            fontFamily: "Impact",
+            fontSize: "32px",
+            color: "#00FF00",
+        }
+
+        this.input.on("pointerdown", () => {
+            this.scene.start("messageScene", {messageIndex: 0});
+        });
         let bg = this.add.sprite(100, 100, 'title').setOrigin(0, 0);
     }
 }
