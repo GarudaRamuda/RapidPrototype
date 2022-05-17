9236e92ea8c12d9982dfb018a33b206dcdc1ff8b
nickpastoria <84762796+nickpastoria@users.noreply.github.com>
2022-05-16 18:25:04 -0700

    Tap Image

--- src/scenes/Title.js
+++ src/scenes/Title.js
@@ -6,6 +6,8 @@ class Title extends Phaser.Scene {
 
     preload() {
         this.load.image('title', './assets/Title.png');
+        this.load.image('tap1', './assets/Tap1.png');
+        this.load.image('tap2', './assets/Tap2.png');
     }
 
     create() {
@@ -15,7 +17,7 @@ class Title extends Phaser.Scene {
         this.logoShadow.setTint(0x000000);
         this.logoShadow.setAlpha(0.25);
         this.logo = this.add.sprite(config.scale.width/2, config.scale.height/3, 'title').setOrigin(0.5, 0.5).setScale(0.5);
-        
+        this.tap1 = this.add.sprite(config.scale.width - 100, config.scale.height - 100, 'tap1').setScale(0.25);
     }
 
     update(time) {
